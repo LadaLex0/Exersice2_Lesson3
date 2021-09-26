f72293c0fd7be79e8881551e97c7de02e8b0b493
LadaLex0 <91010364+LadaLex0@users.noreply.github.com>
2021-09-26 12:39:04 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+a = int(input('Write 1st number to find value: '))
+b = int(input('Write 2nd number to find value: '))
+x = a + b
+if x > 20:
+    print('The value is more than 20')
+elif x == 20:
+    print('The value equal to 20')
+else:
+    print('The value is less than 20')
